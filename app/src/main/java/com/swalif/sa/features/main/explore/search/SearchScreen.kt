6cb2ddcbefa168db38fe8b54a655e8817a2ca0ec
salmanA169 <salman.alamoudi95@gmail.com>
2023-04-17 04:30:12 +0300

    SearchScreen *

--- app/src/main/java/com/swalif/sa/features/main/explore/search/SearchScreen.kt
+++ app/src/main/java/com/swalif/sa/features/main/explore/search/SearchScreen.kt
@@ -1,5 +1,6 @@
 package com.swalif.sa.features.main.explore.search
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.animation.core.CubicBezierEasing
 import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.VectorConverter
@@ -9,27 +10,37 @@ import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.keyframes
 import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.geometry.center
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.Wallpapers
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -37,8 +48,10 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.swalif.sa.R
 import com.swalif.sa.Screens
+import com.swalif.sa.model.UserInfo
 import com.swalif.sa.ui.theme.ChatAppTheme
 import java.lang.Float.max
 import kotlin.math.PI
@@ -50,12 +63,12 @@ fun NavGraphBuilder.searchScreen(navController: NavController) {
     }
 }
 
-@Preview
+@Preview(group = "s1", wallpaper = Wallpapers.GREEN_DOMINATED_EXAMPLE)
+@Preview(group = "s1", wallpaper = Wallpapers.GREEN_DOMINATED_EXAMPLE, uiMode = UI_MODE_NIGHT_YES)
 @Composable
 fun Preview2() {
     ChatAppTheme {
-
-        ImageProgressIndicator()
+        SearchScreen(navController = rememberNavController())
     }
 }
 
@@ -70,7 +83,7 @@ fun SearchScreen(
         ElevatedCard(
             modifier = Modifier
                 .align(Alignment.Center)
-                .size(200.dp)
+                .padding(14.dp)
         ) {
 //            AsyncImage(model = Icons.Default.Email, contentDescription = "",modifier = Modifier.drawBehind {
 //                drawArc(
@@ -78,15 +91,39 @@ fun SearchScreen(
 //                )
 //            })
 
-            ImageProgressIndicator()
+            ImageProgressIndicator(
+                Modifier
+                    .align(CenterHorizontally)
+                    .padding(4.dp))
+            Row(horizontalArrangement = Arrangement.spacedBy(16.dp),modifier = Modifier.padding(16.dp)) {
+                Button(onClick = { /*TODO*/ },colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.errorContainer)) {
+                    Text(text = stringResource(id = R.string.ignore),color = MaterialTheme.colorScheme.onErrorContainer)
+
+                }
+                Button(onClick = { /*TODO*/ }) {
+                    Text(text = stringResource(id = R.string.accept))
+                }
+            }
+
         }
     }
 }
 
 @Composable
-fun ImageProgressIndicator() {
+fun UserStatusSection(
+    modifier :Modifier = Modifier,
+    userInfo: UserInfo? =null
+) {
+    if(userInfo !=null){
+
+    }else{
+
+    }
+}
+@Composable
+fun ImageProgressIndicator(modifier: Modifier = Modifier) {
     val stroke = with(LocalDensity.current) {
-        Stroke(2.dp.toPx(),cap = StrokeCap.Square)
+        Stroke(2.dp.toPx(), cap = StrokeCap.Square)
     }
     val transition = rememberInfiniteTransition()
     val currentRotation = transition.animateValue(
@@ -101,31 +138,35 @@ fun ImageProgressIndicator() {
     )
     val baseRotation = transition.animateFloat(
         initialValue = 0f, targetValue = 286f, animationSpec = infiniteRepeatable(
-        tween(1332,easing = LinearEasing)
+            tween(1332, easing = LinearEasing)
         )
     )
-    val endAngle = transition.animateFloat(initialValue = 0f, targetValue = 290f, animationSpec = infiniteRepeatable(
-        keyframes {
-            durationMillis = (1332 * 0.5).toInt() + (1332 * 0.5).toInt()
-            0f at 0 with CubicBezierEasing(0.4f, 0f, 0.2f, 1f)
-            290f at (1332 * 0.5).toInt()
-        }
-    ))
-    val startAngle = transition.animateFloat(initialValue = 0f, targetValue = 290f, animationSpec = infiniteRepeatable(
-        keyframes {
-            durationMillis = (1332 * 0.5).toInt() + (1332 * 0.5).toInt()
-            0f at (1332 * 0.5).toInt() with CubicBezierEasing(0.4f, 0f, 0.2f, 1f)
-            290f at durationMillis
-        }
-    ))
-    val color =MaterialTheme.colorScheme.primary
+    val endAngle = transition.animateFloat(initialValue = 0f,
+        targetValue = 290f,
+        animationSpec = infiniteRepeatable(
+            keyframes {
+                durationMillis = (1332 * 0.5).toInt() + (1332 * 0.5).toInt()
+                0f at 0 with CubicBezierEasing(0.4f, 0f, 0.2f, 1f)
+                290f at (1332 * 0.5).toInt()
+            }
+        ))
+    val startAngle = transition.animateFloat(initialValue = 0f,
+        targetValue = 290f,
+        animationSpec = infiniteRepeatable(
+            keyframes {
+                durationMillis = (1332 * 0.5).toInt() + (1332 * 0.5).toInt()
+                0f at (1332 * 0.5).toInt() with CubicBezierEasing(0.4f, 0f, 0.2f, 1f)
+                290f at durationMillis
+            }
+        ))
+    val color = MaterialTheme.colorScheme.primary
     val icon = painterResource(id = R.drawable.user_icon)
     val getOffsetIcon = icon.intrinsicSize
+
     Icon(
         painter = painterResource(id = R.drawable.user_icon),
         contentDescription = "",
-        modifier = Modifier
-            .size(50.dp)
+        modifier = modifier
             .drawBehind {
                 val currentRotationOffset = (currentRotation.value * ((286 + 290)) % 360)
                 val sweep = abs(endAngle.value - startAngle.value)
@@ -138,7 +179,8 @@ fun ImageProgressIndicator() {
                     stroke,
                     getOffsetIcon
                 )
-            })
+            }.size(80.dp)
+    )
 }
 
 fun DrawScope.drawIndeterminateProgress(
@@ -149,15 +191,15 @@ fun DrawScope.drawIndeterminateProgress(
     stroke: Stroke,
     offset: Size
 ) {
-    val strokeCapOffset = if (stroke.cap == StrokeCap.Butt){
+    val strokeCapOffset = if (stroke.cap == StrokeCap.Butt) {
         0f
-    }else {
+    } else {
         (180.0 / PI).toFloat() * (strokeWidth / (CircularIndicatorDiameter / 2)) / 2f
     }
     val adjustedStartAngle = startAngle + strokeCapOffset
-    val adjustedSweep = max (sweep,0.1f)
+    val adjustedSweep = max(sweep, 0.1f)
 
-    drawCircularProgress (adjustedStartAngle,adjustedSweep,color,stroke,offset)
+    drawCircularProgress(adjustedStartAngle, adjustedSweep, color, stroke, offset)
 }
 
 fun DrawScope.drawCircularProgress(
@@ -168,20 +210,19 @@ fun DrawScope.drawCircularProgress(
     offset: Size
 ) {
 
-    val diameterOffset = stroke.width/2
+    val diameterOffset = stroke.width / 2
     val arcDimen = size.width - 2 * diameterOffset
-    val offsetI = Offset(center.x - offset.width,center.y-offset.width)
 
-    // TODO: fix this
     drawArc(
-        color = color ,
+        color = color,
         startAngle = adjustedStartAngle,
         sweepAngle = adjustedSweep,
         useCenter = false,
-        topLeft =Offset(diameterOffset, diameterOffset),
-        size = Size(arcDimen,arcDimen),
+        topLeft = Offset(diameterOffset, diameterOffset),
+        size = Size(arcDimen, arcDimen),
         style = stroke
     )
 }
+
 internal val CircularIndicatorDiameter =
     48.dp - 4.dp * 2
---
+++ app/src/main/java/com/swalif/sa/model/UserInfo.kt
@@ -0,0 +1,7 @@
+package com.swalif.sa.model
+
+data class UserInfo(
+    val username:String,
+    val userUid:String,
+    val imageUri:String
+)
